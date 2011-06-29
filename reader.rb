4079a5f6c694c2560bc7281ef7d6fc7d74f473a7
imdizi <jhkgus@naver.com>
2011-06-29 00:50:11 -0700

    이정도에서_마무리_ㅋ.rb @reader

--- reader.rb
+++ reader.rb
@@ -2,9 +2,7 @@ require 'open-uri'
 
 open('http://log.kaist.ac.kr/ESK/ruby-1/urls.txt') do |f|
 	f.readlines.each do |line|
-		open(line) do |f_2|
-			puts f_2.read
-		end
+		puts open(line){|f_2| f_2.read}
 	end
 end
 
