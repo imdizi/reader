204abb6d3f44e6082f60fbead6837a3dddf71bed
imdizi <jhkgus@naver.com>
2011-06-29 01:03:10 -0700

    으아니 답은 뭐다? ㅅ...셉... ㅋ 그림찾느라 힘들었습니다 @reader

--- reader.rb
+++ reader.rb
@@ -4,5 +4,13 @@ open('http://log.kaist.ac.kr/ESK/ruby-1/urls.txt') do |f|
 	f.readlines.each do |line|
 		puts open(line){|f_2| f_2.read}
 	end
+
+	puts "\n"
+	puts "dlrjs"
+	puts "enq"
+	puts "jft"
+	puts "lrd"
+	puts "lqs"
+	puts "lek"
 end
 
