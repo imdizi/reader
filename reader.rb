f88573c6d161058ecf6560386746202b2dfe5038
imdizi <jhkgus@naver.com>
2011-06-29 00:41:38 -0700

    url의 url을 출력 @reader

--- reader.rb
+++ reader.rb
@@ -2,8 +2,9 @@ require 'open-uri'
 
 open('http://log.kaist.ac.kr/ESK/ruby-1/urls.txt') do |f|
 	f.readlines.each do |line|
-		puts line
-		puts 'AAA'
+		open(line) do |f_2|
+			puts f_2.read
+		end
 	end
 end
 
